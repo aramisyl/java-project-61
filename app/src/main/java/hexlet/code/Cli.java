aa0ace0fe252aa17c426ef6c81dda016f6e7ac57
ylaurynovich <y.laurynovich@gmail.com>
2023-03-13 00:32:24 +0100

    update Hello message to allow name input

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void greet() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String name = scanner.nextLine();
+        System.out.printf("Hello, %s!\n", name);
+    }
+}
