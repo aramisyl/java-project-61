f1b241238cbf7f97bdd03c08b2795a63ed24692e
ylaurynovich <y.laurynovich@gmail.com>
2023-06-18 23:12:13 +0200

    task 1 completed
    1. added choices 0 and 1
    2. Separated  cli to get name and greet

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greet();
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n" + "1 - Greet\n" +"0 - Exit");
+        int option = scanner.nextInt();
+        System.out.println("Your choice:" + option);
+
+        switch (option) {
+            case 0:
+                scanner.close();
+                break;
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greet(Cli.getName());
+                scanner.close();
+                break;
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,10 +2,15 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greet() {
+    public static String getName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name?");
         String name = scanner.nextLine();
+        scanner.close();
+        return name;
+    }
+
+    public static void greet(String name) {
         System.out.printf("Hello, %s!\n", name);
     }
 }
